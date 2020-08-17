b19f848624eb4af8ec6436c320792743c524d0e0
Henrik Bengtsson <hb@aroma-project.org>
2020-08-16 17:10:07 -0700

    Use newer version of run.R script

--- incl/bin/run.R
+++ incl/bin/run.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-library("revdepcheck")
+library(revdepcheck.extras)
 options(warn = 1L)
 
 precheck <- function() {
@@ -12,7 +12,7 @@ precheck <- function() {
 #' @importFrom revdepcheck revdep_check
 #' @export
 
-args <- base::commandArgs()
+args <- base::commandArgs(trailingOnly = TRUE)
 if ("--reset" %in% args) {
   revdep_reset()
 } else if ("--todo-reset" %in% args) {
@@ -22,16 +22,24 @@ if ("--reset" %in% args) {
   todo()
 } else if ("--add" %in% args) {
   pos <- which("--add" == args)
+  if (pos == length(args)) stop("Missing value for option '--add'")
   pkgs <- parse_pkgs(args[seq(from = pos + 1L, to = length(args))])
   revdep_add(packages = pkgs)
   todo()
 } else if ("--rm" %in% args) {
   pos <- which("--rm" == args)
+  if (pos == length(args)) stop("Missing value for option '--rm'")
   pkgs <- parse_pkgs(args[seq(from = pos + 1L, to = length(args))])
   revdep_rm(packages = pkgs)
   todo()
 } else if ("--add-broken" %in% args) {
   revdep_add_broken()
+  todo()
+} else if ("--add-error" %in% args) {
+#  res <- revepcheck::revdep_summary()
+  pkgs <- revdep_pkgs_with_status("error")
+  str(pkgs)
+  revdep_add(packages = pkgs)
   todo()
 } else if ("--add-all" %in% args) {
   revdep_init()
@@ -53,6 +61,7 @@ if ("--reset" %in% args) {
   todo()
 } else if ("--show-check" %in% args) {
   pos <- which("--show-check" == args)
+  if (pos == length(args)) stop("Missing value for option '--show-check")
   pkgs <- parse_pkgs(args[seq(from = pos + 1L, to = length(args))])
   for (pkg in pkgs) {
     for (dir in c("old", "new")) {
@@ -73,8 +82,14 @@ if ("--reset" %in% args) {
   cat(sprintf("[n=%d] %s\n", length(pkgs), paste(pkgs, collapse = " ")))
 } else if ("--list-error" %in% args) {
   cat(paste(revdep_pkgs_with_status("error"), collapse = " "), "\n", sep="")
+} else if ("--list-failure" %in% args) {
+  cat(paste(revdep_pkgs_with_status("failure"), collapse = " "), "\n", sep="")
 } else if ("--add-error" %in% args) {
   revdepcheck::revdep_add(packages = revdep_pkgs_with_status("error"))
+} else if ("--add-failure" %in% args) {
+  revdepcheck::revdep_add(packages = revdep_pkgs_with_status("failure"))
+} else if ("--preinstall-update" %in% args) {
+  revdep_preinstall_update()
 } else if ("--preinstall-children" %in% args) {
   pkg <- revdep_this_package()
   pkgs <- revdepcheck:::cran_revdeps(pkg)
@@ -82,16 +97,19 @@ if ("--reset" %in% args) {
 } else if ("--preinstall-error" %in% args) {
   res <- revdepcheck::revdep_summary()
   revdep_preinstall(revdep_pkgs_with_status("error"))
+} else if ("--preinstall-failure" %in% args) {
+  res <- revdepcheck::revdep_summary()
+  revdep_preinstall(revdep_pkgs_with_status("failure"))
 } else if ("--preinstall-todo" %in% args) {
   todo <- revdep_todo()
   revdep_preinstall(todo$package)
 } else if ("--preinstall" %in% args) {
   pos <- which("--preinstall" == args)
+  if (pos == length(args)) stop("Missing value for option '--preinstall'")
   pkgs <- parse_pkgs(args[seq(from = pos + 1L, to = length(args))])
   revdep_preinstall(pkgs)
 } else {
-  print(args)
   stopifnot(length(args) == 0L)
   check()
-  revdep_report(all = TRUE)
+  revdepcheck::revdep_report(all = TRUE)
 }
